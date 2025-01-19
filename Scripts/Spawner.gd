8ef3f0ba94e7eb8af41b5df7c03a374c268ab32e
dinpixels <dinpixels@proton.me>
2025-01-19 21:50:10 +0800

    Added VisibilityNotifier2D to Spawner to prevent auto spawn in Editor and be more flexible overall

--- Scripts/Spawner.gd
+++ Scripts/Spawner.gd
@@ -2,6 +2,7 @@ tool
 extends Node2D
 
 
+export (bool) var allow_spawn := false setget set_allow_spawn, is_spawn_allowed
 export (bool) var safe := true setget set_safe_type, is_safe
 export (int, 0, 1) var no := 0 setget set_item_no, get_item_no
 export (int, -1, 1) var direction := -1
@@ -18,10 +19,11 @@ onready var timer := $Timer
 
 func _ready() -> void:
 	#if not OS.is_debug_build():
-		_spawn()
+	if allow_spawn == true:
+		spawn()
 
 
-func _spawn() -> void:
+func spawn() -> void:
 	timer.start(seconds)
 
 	var obj = scenes[no].instance()
@@ -53,5 +55,14 @@ func get_item_no() -> int:
 	return no
 
 
+func set_allow_spawn(allow: bool) -> void:
+	allow_spawn = allow
+
+
+func is_spawn_allowed() -> bool:
+	return allow_spawn
+
+
 func _on_timeout() -> void:
-	_spawn()
+	if allow_spawn == true:
+		spawn()
--- Scripts/SpawnerSprite.gd
+++ Scripts/SpawnerSprite.gd
@@ -2,3 +2,13 @@ tool
 extends Sprite
 
 # Tool enabled to update the sprite when parent's values are adjusted on the editor
+
+
+func _on_viewport_entered(_viewport) -> void:
+	owner.set_allow_spawn(true)
+	owner.spawn()
+
+
+func _on_viewport_exited(_viewport) -> void:
+	if not OS.is_debug_build():
+		queue_free()
